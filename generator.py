ed71b07f1d8286243e9a1f3a4f049a4c03a90eff
Ian Wood <ianwood103@gmail.com>
2021-03-13 17:08:38 -0500

    refined code and dataset efficiency

--- generator.py
+++ generator.py
@@ -1,10 +1,12 @@
-import inflect
-import csv
 import json
 import random
 
-engine = inflect.engine();
 animals = json.load(open("animals.txt"))
 adjectives = json.load(open("adjectives.txt"))
 
-print("The " + adjectives[random.randint(0,len(adjectives))].split(" ")[1].capitalize() + " " + engine.plural(animals[random.randint(0,len(animals))]).split(" ")[-1])
+randomAdj = ""
+if (random.randint(1, 2) == 1):
+    randomAdj = adjectives[random.randint(0,len(adjectives))] + " "
+randomAnimal = animals[random.randint(0,len(animals))]
+
+print("The " + randomAdj + randomAnimal)
